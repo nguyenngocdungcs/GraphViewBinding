97ee35b0fcbc7eb34f011020d0a00a67f9fc43ea
Dung Nguyen <nguyenngocdungcs@gmail.com>
2015-09-15 23:52:37 +0700

    First commit

---
+++ GraphViewDemo/MainActivity.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Com.Jjoe64.Graphview;
+using Com.Jjoe64.Graphview.Series;
+
+namespace GraphViewDemo
+{
+	[Activity (Label = "GraphViewDemo", MainLauncher = true, Icon = "@drawable/icon")]
+	public class MainActivity : Activity
+	{
+		GraphView _graph;
+//		ViewPort _ds;
+
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			// Set our view from the "main" layout resource
+			SetContentView (Resource.Layout.Main);
+			_graph = FindViewById <GraphView> (Resource.Id.graph);
+
+			var points = StepModel.Mockup ().Select (
+				(x, i) => new DataPoint (i, x.Steps)
+			).ToArray ();
+
+			BarGraphSeries series = new BarGraphSeries (points);
+			_graph.AddSeries (series);
+		}
+	}
+}
+
+
---
+++ GraphViewDemo/StepModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GraphViewDemo
+{
+	public class StepModel
+	{
+		public DateTime Date { get; set; }
+		public Int64 Steps { get; set; }
+
+		public StepModel ()
+		{
+			Date = DateTime.Now;
+			Steps = 0;
+		}
+
+		public static List<StepModel> Mockup () {
+			var res = new List<StepModel> ();
+			res.Add (new StepModel {
+				Date = new DateTime (2015, 9, 1),
+				Steps = 1213
+			});
+			res.Add (new StepModel {
+				Date = new DateTime (2015, 9, 2),
+				Steps = 800
+			});
+			res.Add (new StepModel {
+				Date = new DateTime (2015, 9, 3),
+				Steps = 2121
+			});
+			res.Add (new StepModel {
+				Date = new DateTime (2015, 9, 4),
+				Steps = 400
+			});
+			res.Add (new StepModel {
+				Date = new DateTime (2015, 9, 5),
+				Steps = 678
+			});
+
+			return res;
+		}
+	}
+}
+
