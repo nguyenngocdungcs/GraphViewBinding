34111abd3e70733ebd88b8d2edd4d54dad4e2894
Dung Nguyen <nguyenngocdungcs@gmail.com>
2015-09-30 22:23:14 +0700

    Fix 28 warnings following this instruction: https://forums.xamarin.com/discussion/comment/154664/#Comment_154664

--- GraphViewDemo/MainActivity.cs
+++ GraphViewDemo/MainActivity.cs
@@ -6,8 +6,9 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
-using Com.Jjoe64.Graphview;
-using Com.Jjoe64.Graphview.Series;
+using AndroidGraphView;
+using AndroidGraphView.Series;
+using Android.Graphics;
 
 namespace GraphViewDemo
 {
@@ -15,7 +16,6 @@ namespace GraphViewDemo
 	public class MainActivity : Activity
 	{
 		GraphView _graph;
-//		ViewPort _ds;
 
 		protected override void OnCreate (Bundle bundle)
 		{
@@ -26,13 +26,42 @@ namespace GraphViewDemo
 			_graph = FindViewById <GraphView> (Resource.Id.graph);
 
 			var points = StepModel.Mockup ().Select (
-				(x, i) => new DataPoint (i, x.Steps)
+				(x, i) => new DataPoint (
+					new Java.Util.Date (x.Date.Ticks),
+					x.Steps)
 			).ToArray ();
 
 			BarGraphSeries series = new BarGraphSeries (points);
+			series.Spacing = 20;
+			series.DrawValuesOnTop = true;
+			series.ValuesOnTopColor = (int)Color.Yellow;
+			series.Color = (int)Color.Yellow;
+			
+			_graph.GridLabelRenderer.LabelFormatter = new DateLabelFormat ();
+			_graph.GridLabelRenderer.NumHorizontalLabels = 5;
 			_graph.AddSeries (series);
-		}
 
+			_graph.Viewport.SetMaxY (points.Max (x => x.GetY()) + 100);
+			_graph.Viewport.SetMinY (0);
+			_graph.Viewport.YAxisBoundsManual = true;
+
+			_graph.Viewport.SetMaxX (points.Max (x => x.GetX()) + 100);
+			_graph.Viewport.SetMinX (points.Min (x => x.GetX()) - 1000);
+			_graph.Viewport.XAxisBoundsManual = true;
+		}
+	}
+
+	public class DateLabelFormat : DefaultLabelFormatter
+	{
+		public override string FormatLabel (double p0, bool p1)
+		{
+			if (p1) { // isValueX
+				var date = new DateTime ((long) p0);
+				return date.ToString ("MMM d");
+			}
+
+			return base.FormatLabel (p0, p1);
+		}
 	}
 }
 
